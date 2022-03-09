d36eb51ecb849f3f14c471a8e4dc52efb03ed796
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-09 16:20:10 +0500

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,17 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+import random
+
+len_name = 0
+long_name = []
+
+max_name = max(names, key=len)
+max_name = len(max_name)
+
+for name in names:
+    if (len(name)) >= max_name:
+        long_name.append(name)
+
+print(random.choice(long_name))
