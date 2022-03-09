9e7e053686d3dc00358a66e9687b2b2b93648c96
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-09 16:25:05 +0500

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,23 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+numbers = []
+numbers_sqrt = []
+i = 1
+n = random.randint(10, 100)
+
+#numbers = [-20, -3, -1, 0, 1, 3, 20, 25, -25]
+#numbers = [2, -5, 8, 9, -25, 25, 4]
+
+while i <= n:
+    numbers.append(random.randint(-100, 100))
+    i += 1
+
+for number in numbers:
+    if (number >= 4) and ((number ** (0.5)) % 1 == 0):
+    # if (number >= 4) and ((number ** (0.5)).is_integer()):
+        numbers_sqrt.append(int(number ** (0.5)))
+
+print(numbers_sqrt)
