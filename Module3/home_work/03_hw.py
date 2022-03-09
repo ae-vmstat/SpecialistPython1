aa79bf0770fba22fb87cc5a936142f58ee14de2f
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-09 11:58:45 +0500

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,22 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+
+import random
+numbers = []
+numbers_sum = 0
+i = 1
+
+n = 45
+
+while i <= n:
+    numbers.append(random.randint(-100, 100))
+    i += 1
+
+for number in numbers:
+    if (number >= 1) and (number % 2 == 0):
+        numbers_sum += number
+
+# if numbers_sum % 2 == 0:
+#     print("OK")
+print(numbers_sum)
