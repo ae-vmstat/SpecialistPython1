c289731cb5990f6b7e1fd323c65204c44a49a698
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-16 10:21:42 +0500

    Update 03_hw_func.py

--- Module4/home_work/03_hw_func.py
+++ Module4/home_work/03_hw_func.py
@@ -2,3 +2,11 @@
 # Находится ли одна окружность целиком внутри другой
 
 # TODO: your code here
+
+def circles (x1, y1, x2,y2, R1, R2):
+    return (((x1 - x2)**2 + (y1 - y2)**2) < ((R1 - R2)**2))
+
+x1, y1 = 20, 16
+x2, y2 = 2, 4
+R1, R2 = 60, 20
+print(circles(x1, y1, x2, y2, R1, R2))
