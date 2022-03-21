195e8fce0189313a2a7c16c47ed156db3f410e3c
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-21 11:16:02 +0500

    Update 01_hw_file.py

--- Module6/home_work/01_hw_file.py
+++ Module6/home_work/01_hw_file.py
@@ -1,8 +1,8 @@
 # Напишите функцию log() принимающую в качестве аргумента строку и дописывающую это строку в конец файла
 
 def log(text, file="log.txt"):
-    pass
-
+    with open(file, 'a') as f:
+        f.write(text + '\n')
 
 log("hello world")  # дописывает "hello world" в конец файла log.txt
 log("message", "log01.txt")  # дописывает "message" в конец файла log01.txt
