2243c7a45e1ab46ce1ec98f9aac4682acc7334a2
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-21 11:17:56 +0500

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,9 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    total = 0
+    for line in f:
+        if line.strip().isdigit():
+            total += int(line)
+
+print(total)
