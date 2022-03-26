53d833b13827bb0a3a72535a17d14765452f0991
ae-vmstat <100474823+ae-vmstat@users.noreply.github.com>
2022-03-26 18:42:06 +0500

    Update 04_task_regexp.py

--- Module9/practice/regexp/04_task_regexp.py
+++ Module9/practice/regexp/04_task_regexp.py
@@ -2,3 +2,12 @@
 text = "World War II began on Sep 1939 and ended on Sep 1945. Victory Day has been celebrated since 1945."
 # Найдите и выведите все года, которым предшествует месяц.
 # Уточнение: вывести ТОЛЬКО года, но те, ПЕРЕД которыми стоит месяц!
+
+import re
+
+years = (re.findall(r'\b\w{3} \d{4}\b',text))
+
+for year in years:
+    year_only = re.findall(r'\b\d{4}\b', year)
+    print(year_only)
+    
